e59027fc42b285727fc68a7137f1779cc8e37cba
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-03 13:38:54 -0800

    added body parser

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express'),
     app = express(),
+    port = process.env.PORT || 3000,
     todoRoutes = require('./routes/todos'),
-    port = process.env.PORT || 3000;
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/api/todos', todoRoutes);
 
 app.get('/', function(req, res) {
     res.send('Hello from the root route!');
 });
-
-app.use('/api/todos', todoRoutes);
 
 app.listen(port, function() {
     console.log('App is running!');
--- routes/todos.js
+++ routes/todos.js
@@ -9,7 +9,17 @@ router.get('/', function(req, res) {
     })
     .catch(function(err) {
         res.send(err);
+    });
+});
+
+router.post('/', function(req, res) {
+    db.Todo.create(req.body)
+    .then(function(newTodo) {
+        res.status(201).json(newTodo);
     })
+    .catch(function(err) {
+        res.send(err);
+    });
 });
 
 module.exports = router;
