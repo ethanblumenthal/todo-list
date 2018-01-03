08412e7b01241c3a86e0a9cb599f2958784b1d6a
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-03 15:06:23 -0800

    appended todos to page via api

---
+++ public/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $.getJSON('/api/todos').then(addTodos);
+});
+
+function addTodos(todos) {
+    todos.forEach(function(todo) {
+        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    });
+}
