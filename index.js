09dbd9689141fc8cb552bb82d4c0f1a064589ce1
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-03 13:20:21 -0800

    added todo route

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express'),
     app = express(),
+    todoRoutes = require('./routes/todos'),
     port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send('Hi from express!');
+    res.send('Hello from the root route!');
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(port, function() {
     console.log('App is running!');
