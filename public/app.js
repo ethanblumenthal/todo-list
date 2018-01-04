ae5b7a2b085b83040fb6f3d34f24b3feb29a7b27
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-03 16:07:02 -0800

    added completed functionality

--- public/app.js
+++ public/app.js
@@ -5,7 +5,13 @@ $(document).ready(function() {
             createTodo();
         }
     });
-    $('.list').on('click', 'span', function() {
+
+    $('.list').on('click', 'li', function() {
+        updateTodo($(this));
+    });
+
+    $('.list').on('click', 'span', function(evt) {
+        evt.stopPropagation();
         removeToDo($(this).parent());
     });
 });
@@ -19,6 +25,7 @@ function addTodos(todos) {
 function addTodo(todo) {
     var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if (todo.completed) {
         newTodo.addClass('done');
     }
@@ -46,6 +53,24 @@ function removeToDo(todo) {
     })
     .then(function(data) {
         todo.remove();
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+}
+
+function updateTodo(todo) {
+    var updateUrl = '/api/todos/' + todo.data('id');
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: 'PUT',
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updateTodo) {
+        todo.toggleClass('done');
+        todo.data('completed', isDone);
     })
     .catch(function(err) {
         console.log(err);
