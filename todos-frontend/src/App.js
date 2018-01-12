33293b7af31b58f5e882d6f1199db122aa963a31
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-01-11 16:48:56 -0800

    implemented fetch, componenentWillMount, and error handling for api

--- todos-frontend/src/App.js
+++ todos-frontend/src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import TodoList from './TodoList';
 import './App.css';
+const APIURL = '/api/todos';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentWillMount() {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    fetch(APIURL)
+    .then(res => {
+      if (!res.ok) {
+        if (res.status >= 400 && res.status < 500) {
+          return res.json().then(data => {
+            let err = {errorMessage: data.message};
+            throw err;
+          })
+        } else {
+          let err = {errorMessage: 'Please try again later, server is not responding'};
+          throw err;
+        }
+      }
+      return res.json()
+    })
+    .then(todos => this.setState({todos}));
+  }
+
   render() {
     return (
       <div className="App">
